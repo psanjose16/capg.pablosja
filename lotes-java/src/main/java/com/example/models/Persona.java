5e2789f9d3b6125d8ec652a00dd2228f20707e00
psanjose16 <pablo.san-jose-acedo@capgemini.com>
2025-03-20 13:03:27 +0100

    added openAPI code to demo

--- lotes-java/src/main/java/com/example/models/Persona.java
+++ lotes-java/src/main/java/com/example/models/Persona.java
@@ -4,6 +4,18 @@ import lombok.Data;
 
 @Data
 public class Persona {
+	public Persona(long id, String nombre, String correo, String ip) {
+		this.id = id;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.ip = ip;
+	}
+	public Persona(long id, String nombre, String correo, String ip) {
+		this.id = id;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.ip = ip;
+	}
 	private long id;
 	private String nombre, correo, ip;
 	// Ctor(), Get/Set, toString(), …
